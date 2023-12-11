ef8fcfe60d3f3c3b7f357d8f0117415975bdbe17
racky7 <raj.chauhan000@gmail.com>
2023-12-11 21:40:30 +0530

    final changes

---
+++ README.md
@@ -0,0 +1,21 @@
+# Simple React Code Editor
+
+Deployed URL - 
+
+## Local Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/racky7/simple-react-code-editor
+2. Navigate to the project's directory:
+
+   ```bash
+   cd simple-react-code-editor
+3. Install dependencies:
+   ```bash
+   npm install
+4. To run the project locally:
+   ```bash
+   npm run dev
+   
---
+++ src/screens/MainPage.tsx
@@ -0,0 +1,23 @@
+import { ChangeEvent, useState } from "react";
+import CodeEditor from "../components/CodeEditor";
+import { defaultCode } from "../constants";
+import "./mainpage.style.css";
+
+const MainPage = () => {
+  const [code, setCode] = useState<string>(defaultCode);
+
+  const onCodeChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setCode(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>simple react code editor</h1>
+      <div className="editor-box">
+        <CodeEditor code={code} onCodeChange={onCodeChange} />
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
